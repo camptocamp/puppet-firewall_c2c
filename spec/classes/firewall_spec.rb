0bae46ee4c9c449de545b4d6399596bacf07a2ec
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-12-13 10:26:33 +0100

    Fix unit tests

--- spec/classes/firewall_spec.rb
+++ spec/classes/firewall_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe 'firewall' do
 
-  let :facts do
-    {
-      :kernel                 => 'Linux',
-      :operatingsystem        => 'Debian',
-      :operatingsystemrelease => 'wheezy',
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        if facts[:lsbdistcodename] == 'squeeze'
+          facts.merge({ :iptables_persistent_version => '0.0.20100801', }) 
+        else
+          facts
+        end
+      end
 
-  let :pre_condition do
-    "firewall { '000 accept all icmp':
+      let :pre_condition do
+        "firewall { '000 accept all icmp':
       proto   => 'icmp',
       action  => 'accept',
     }
@@ -25,17 +27,19 @@ describe 'firewall' do
       state => ['RELATED', 'ESTABLISHED'],
       action  => 'accept',
     }"
+      end
+
+      it { should compile.with_all_deps }
+      it { should have_firewall_resource_count(3) }
+      it {
+        skip("that_requires doesn't work with autorequire")
+        should contain_firewall('001 accept all to lo interface').
+          that_requires('Firewall[000 accept all icmp]') }
+      it {
+        skip("that_requires doesn't work with autorequire")
+        should contain_firewall('002 accept related established rules').
+          that_requires('Firewall[001 accept all to lo interface]') }
+
+    end
   end
-
-  it { should compile.with_all_deps }
-  it { should have_firewall_resource_count(3) }
-  it {
-    skip("that_requires doesn't work with autorequire")
-    should contain_firewall('001 accept all to lo interface').
-      that_requires('Firewall[000 accept all icmp]') }
-  it {
-    skip("that_requires doesn't work with autorequire")
-    should contain_firewall('002 accept related established rules').
-      that_requires('Firewall[001 accept all to lo interface]') }
-
 end
